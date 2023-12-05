de33909b35c20a015725e0b3be5e3dd5b4ed7a51
sara_bentafat <s_bentafat@estin.dz>
2023-12-05 10:34:15 +0100

    fix the front content (home page) in its folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
+import Home from './Components/Home'; 
+import './App.css';
 
 function App() {
   return (
-    <div className="bg-black text-white text-5xl h-screen">
-Its working wiiiiiwwwwwwwww 
-    </div>
+    <Home/>
   );
 }
 
---
+++ frontend/src/Components/Nav.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import SearchBar from "./SearchBar";
+
+
+function Nav() {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+
+  return (
+    <nav className="main-nav">
+      <ul className="flex space-x-12 absolute ml-60 mb-30 p-3 font-serif text-lg">
+        <a
+          className={`mt-3 hover:text-yellow-600 hover:border-yellow-600 border-b-2 ${activeItem === 'Home' ? 'text-yellow-600 border-yellow-600 border-b-2' : ''}`}
+          href="/"
+          onClick={() => handleItemClick('Home')}
+        >
+          Home
+        </a>
+        <a
+          className={`mt-3 hover:text-yellow-600 hover:border-yellow-600 border-b-2 ${activeItem === 'PostAnnonce' ? 'text-yellow-600 border-yellow-600 border-b-2' : ''}`}
+          href="/"
+          onClick={() => handleItemClick('PostAnnonce')}
+        >
+          Post Annonce
+        </a>
+        <a
+          className={`mt-3 hover:text-yellow-600 hover:border-yellow-600 border-b-2 ${activeItem === 'GeneratePrice' ? 'text-yellow-600 border-yellow-600 border-b-2' : ''}`}
+          href="/"
+          onClick={() => handleItemClick('GeneratePrice')}
+        >
+          Generate Price
+        </a>
+        <a
+          className={`mt-3 hover:text-yellow-600 hover:border-b-2 border-yellow-600 ${activeItem === 'ContactUs' ? 'text-yellow-600 border-b-2 border-yellow-600' : ''}`}
+          href="/"
+          onClick={() => handleItemClick('ContactUs')}
+        >
+          Contact Us
+        </a>
+        <SearchBar/>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
