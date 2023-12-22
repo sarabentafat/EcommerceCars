36614cf40ff82bb2b1e0e4dd71f2fddbf98fd2c1
sara_bentafat <s_bentafat@estin.dz>
2023-12-22 20:29:20 +0100

    change the local db to cloud mongodb

--- backend/config/connectToDb.js
+++ backend/config/connectToDb.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 module.exports=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_CLOUD_URL)
         console.log("connected to mongodb ^-^ ")
         
     }catch{
